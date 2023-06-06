1924d7587f58b8495aad36ff767f6d5afd6f886a
miner <miner2@kali.ar.mak.ac.ug>
2023-06-07 01:23:22 +0300

    0x00-python-hello_world

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+i = [p for p in range(1, 100)]
+print(i)
+for integ in i:
+    if integ % 3 == 0 and integ % 5 == 0:
+        print("FizzBuzz")
+    elif integ % 3 == 0:
+        print("Fizz")
+    elif integ % 5 == 0:
+        print("Buzz")
+    else:
+        print(integ)
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def uppercase(string):
+    for char in string:
+        u_char = chr(ord(char) - 32) if ord('a')
+        <= ord(char) <= ord('z') else char
+        print(u_char, end='')
+    print()
