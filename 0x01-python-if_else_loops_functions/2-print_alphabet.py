7c24c9e461524c141093931365fb0309040c3054
miner <miner2@kali.ar.mak.ac.ug>
2023-06-06 16:13:01 +0300

    0x00-python-hello_world

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-letters = [chr(letter_code) for letter_code in range(ord('a'), ord('z')+1)]
-print(', '.join(letters))
+for ch in range(ord('a'), ord('z')+1):
+    print(f"{chr(ch)}", end='')
+
