4491e1a965cd45bc68fccb45f04bfb045330f2b3
okatajames <jamesokata3@gmail.com>
2023-06-14 01:37:20 +0300

    # 0x02. Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif track == 1:
         print("1 argument:")
-    elif:
+    else:
         print("{} arguments:".format(track))
     for k in range(track):
         print("{}: {}".format(k + 1, sys.argv[k + 1]))
