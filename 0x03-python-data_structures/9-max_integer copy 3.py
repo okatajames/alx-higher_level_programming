d4c60034315982f769360dddebffae29baab1565
okatajames <jamesokata3@gmail.com>
2023-06-14 23:16:49 +0300

    # 0x03. Python - python Data structures

--- 0x03-python-data_structures/9-max_integer copy 3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""A func that returns the max value in a list"""
-
-
-def max_integer(my_list=[]):
-    my_list.sort(reverse=True)
-    max_int = my_list[0]
-    return (max_int)
