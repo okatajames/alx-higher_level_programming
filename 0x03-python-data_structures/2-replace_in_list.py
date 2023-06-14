fb23b4cbae333d0ce1fdc32f5d38da866d115d54
okatajames <jamesokata3@gmail.com>
2023-06-14 03:24:14 +0300

    # 0x03. Python - python Data structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """A function that replaces an element of a list in a specific position"""
+
 
 def replace_in_list(my_list, idx, element):
     if idx < 0:
@@ -12,4 +14,4 @@ def replace_in_list(my_list, idx, element):
 
     my_list[idx] = element
 
-    return(my_list)
+    return (my_list)
